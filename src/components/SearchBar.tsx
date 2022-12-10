244cfc2d280fb2a8ceb234e4341dc1949ce78138
Lewis Barnes <lewis_barnes@live.co.uk>
2022-12-10 12:43:53 +0000

    add opening regular links from search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,24 @@
 import { useEffect } from 'preact/hooks';
 import { JSXInternal } from 'preact/src/jsx';
 
+const urlRegEx =
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
 export const SearchBar = () => {
   const search = (e: JSXInternal.TargetedKeyboardEvent<HTMLInputElement>) => {
     if (e.key != 'Enter') {
       return;
     }
-    window.open(`https://duckduckgo.com/?q=${e.currentTarget.value}`, '_blank');
+    if (urlRegEx.test(e.currentTarget.value)) {
+      window.open(
+        e.currentTarget.value.startsWith('http://') || e.currentTarget.value.startsWith('https://')
+          ? e.currentTarget.value
+          : 'http://' + e.currentTarget.value,
+        '_blank'
+      );
+    } else {
+      window.open(`https://duckduckgo.com/?q=${e.currentTarget.value}`, '_blank');
+    }
   };
 
   useEffect(() => {
