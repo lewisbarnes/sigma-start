b11064785bee2ec1419e9d7c50ff24f533a7503d
Lewis Barnes <lewis_barnes@live.co.uk>
2022-12-12 14:20:53 +0000

    change border color on quicklinks

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -9,11 +9,11 @@ export const QuickLinks = () => {
   return (
     <div class="flex gap-4 mx-auto w-max text-white items-center">
       {links.map((link) => (
-        <a class="rounded-full px-2 border" href={link.url} target="_blank">
+        <a class="rounded-full px-2 border border-zinc-500" href={link.url} target="_blank">
           {link.title}
         </a>
       ))}
-      <button class="rounded-full border font-mono px-2">+</button>
+      <button class="rounded-full border border-zinc-500 font-mono px-2 aspect-square">+</button>
     </div>
   );
 };
