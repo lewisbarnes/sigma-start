0ed3ef5bd16c0703d50bc04d559df08dfaa74117
Lewis Barnes <lewis_barnes@live.co.uk>
2022-12-12 17:47:08 +0000

    changed number of days shown in calendar and changed day names

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -5,22 +5,20 @@ export const Calendar = () => {
   const lastMonth = date.clone().set('month', date.month() - 1);
   const firstDay = date.clone().set('date', 1);
 
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
   return (
     <div class="max-w-[50vmin] mx-auto bg-black/60 rounded-md">
       <div class="text-white text-center mx-auto rounded-t-md py-2 text-xl font-bold">
         {date.format('MMMM')}
       </div>
       <div class="text-white grid grid-cols-7 rounded-b-md  text-center py-2 text-xs">
-        <span>Sunday</span>
-        <span>Monday</span>
-        <span>Tuesday</span>
-        <span>Wednesday</span>
-        <span>Thursday</span>
-        <span>Friday</span>
-        <span>Saturday</span>
+        {days.map((x) => (
+          <span>{x.substring(0, 3)}</span>
+        ))}
       </div>
       <div class="mx-auto text-white grid grid-cols-7 rounded-b-md text-xs">
-        {Array.from(new Array(35)).map((x, i) => (
+        {Array.from(new Array(42)).map((x, i) => (
           <p
             class={`${
               (i + 1) % 7 ? 'border-r' : ''
@@ -35,7 +33,7 @@ export const Calendar = () => {
                 </span>
               ) : i + 1 - firstDay.day() == date.date() ? (
                 <div className="bg-white text-black rounded-full w-6 aspect-square flex -mt-1 -ml-1 justify-center items-center">
-                  <p className="block">{i + 1 - firstDay.day()}</p>
+                  <p className="">{i + 1 - firstDay.day()}</p>
                 </div>
               ) : (
                 <span>{i + 1 - firstDay.day()}</span>
